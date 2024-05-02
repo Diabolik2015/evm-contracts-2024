f16222b3fb591646f208f04453b72355e85ab5d2
Marco <mmanzi@soldo.com>
2024-05-02 16:11:03 +0200

    Added the bank for usdt test

---
+++ contracts/utils/UsdtTestBank.sol
@@ -0,0 +1,18 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract UsdtTestBank  {
+    address usdtAddress;
+    constructor(address _usdt) {}
+
+    function getOneHundredDollars() public {
+        SafeERC20.safeTransferFrom(IERC20(usdtAddress), address(this), msg.sender, 100 * 10 ** 18);
+    }
+    function getOneThousandsDollars() public {
+        SafeERC20.safeTransferFrom(IERC20(usdtAddress), address(this), msg.sender, 1000 * 10 ** 18);
+    }
+}
--- deploy/03-deploy-test-usdt.ts
+++ deploy/03-deploy-test-usdt.ts
@@ -1,16 +1,29 @@
 import {DeployFunction} from "hardhat-deploy/dist/types";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {LotteryMaster, TestUsdt} from "../typechain-types";
 
 const deployTestUsdt: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deploy } = hre.deployments;
     const { deployer } = await hre.getNamedAccounts();
 
-    await deploy("TestUsdt", {
+    const usdtDeployed = await deploy("TestUsdt", {
         from: deployer,
         log: true,
         args: [],
         nonce: "pending",
     });
+
+    const bankDeployed = await deploy("UsdtTestBank", {
+        from: deployer,
+        log: true,
+        args: [usdtDeployed.address],
+        nonce: "pending",
+    });
+    const usdtMasterFactory = await hre.ethers.getContractFactory("TestUsdt");
+    const usdtContract = usdtMasterFactory.attach(usdtDeployed.address) as TestUsdt;
+    if (await usdtContract.balanceOf(bankDeployed.address) == BigInt(0)) {
+        await usdtContract.transfer(bankDeployed.address, BigInt(10 ** 6 * 10 ** 18));
+    }
 };
 
 export default deployTestUsdt;
