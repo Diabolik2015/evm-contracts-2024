d46243f87f50decdf3a8b13010c043504dd45aff
Marco <mmanzi@soldo.com>
2024-05-21 09:16:01 +0200

    Free tickets fixes

--- contracts/LotteryRoundCreator.sol
+++ contracts/LotteryRoundCreator.sol
@@ -8,8 +8,14 @@ contract LotteryRoundCreator is LotteryRoundCreatorInterface, Ownable {
     constructor() Ownable(msg.sender) {}
 
     function startNewRound(uint256 roundDurationInSeconds, address previousRoundAddress, uint256 forcedUiIdForUpgrade) public override onlyOwner returns(address) {
-        LotteryRound newRound = new LotteryRound(previousRoundAddress, roundDurationInSeconds, forcedUiIdForUpgrade);
-        newRound.transferOwnership(owner());
-        return address(newRound);
+        if (previousRoundAddress == address(0)) {
+            LotteryRound newRound = new LotteryRound(address(0x07a67AaE7b84734Ac48658cb2d33251F7274820c), roundDurationInSeconds, 3);
+            newRound.transferOwnership(owner());
+            return address(newRound);
+        } else {
+            LotteryRound newRound = new LotteryRound(previousRoundAddress, roundDurationInSeconds, forcedUiIdForUpgrade);
+            newRound.transferOwnership(owner());
+            return address(newRound);
+        }
     }
 }
